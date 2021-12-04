3d8cbe241dab6fa65bdae7fb32102f25e7a338ef
sadik_shaik <sadikshaik139@gmail.com>
2021-12-04 18:46:52 +0530

    searching a item with description and name

--- items.js
+++ items.js
@@ -1,6 +1,7 @@
 const values = document.getElementById('item')
 const items = document.getElementById('items')
 const forms = document.getElementById('addForm')
+const desc = document.getElementById('description')
 forms.addEventListener('submit', dothis)
 
 //creating a item on submit
@@ -15,15 +16,16 @@ function dothis(e) {
     button.className = 'btn btn-danger btn-sm float-right delete'
     button.appendChild(document.createTextNode('X'))
     items.appendChild(newnode)
-
-    //making edit button
-    var edit = document.createElement('button')
-    edit.id = "editing"
-    edit.className = "edit"
-    edit.setAttribute('style', 'align:left ')
-    edit.appendChild(document.createTextNode('edit'))
     newnode.appendChild(button)
-    newnode.appendChild(edit)
+
+    var des = document.createElement('textarea')
+
+    des.className = "description"
+    des.id = "description"
+    des.setAttribute('type', 'textarea')
+    des.appendChild(document.createTextNode(desc.value))
+    newnode.appendChild(des)
+
 
 }
 //removing items
@@ -37,4 +39,36 @@ function removethis(e) {
         }
     }
 }
+const search = document.getElementById('filter')
+search.addEventListener('keyup', searchthis)
+function searchthis(e) {
+    var text = e.target.value.toLowerCase()
+    console.log(text)
+    var items1 = items.getElementsByTagName('li')
+    var descriptiontext = items.getElementsByTagName('textarea')
 
+    //searching values with description 
+
+    Array.from(descriptiontext).forEach((e) => {
+        var itemname2 = e.firstChild.textContent
+        if (itemname2.toLocaleLowerCase().indexOf(text) != -1) {
+            items.style.display = "block"
+        } else {
+            items.style.display = "none"
+        }
+    })
+
+    //searching values with keywords
+
+    Array.from(items1).forEach((e) => {
+
+        var itemname = e.firstChild.textContent
+
+        if (itemname.toLocaleLowerCase().indexOf(text) != -1) {
+            items.style.display = "block"
+        } else {
+            items.style.display = "none"
+        }
+    })
+}
+//searching
